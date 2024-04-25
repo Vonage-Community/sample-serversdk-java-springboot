3c4deed99de1937f69c53dbc0e087442e7ed18f8
Sina Madani <sina.madani@vonage.com>
2024-04-25 17:36:46 +0100

    Add snippet renderer to Verify

--- src/main/java/com/vonage/sample/serversdk/springboot/ApplicationController.java
+++ src/main/java/com/vonage/sample/serversdk/springboot/ApplicationController.java
@@ -1,8 +1,19 @@
 package com.vonage.sample.serversdk.springboot;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+import javax.tools.*;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.io.StringWriter;
+import java.net.URI;
+import java.net.URLClassLoader;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class ApplicationController extends VonageController {
