04ea4d96bbae987eedda96a9f62e62696905de35
Sina Madani <sina.madani@vonage.com>
2024-10-09 10:40:40 +0100

    Add copyright headers

--- src/main/java/com/vonage/sample/serversdk/springboot/package-info.java
+++ src/main/java/com/vonage/sample/serversdk/springboot/package-info.java
@@ -19,19 +19,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
+/**
+ * Demo application using the Vonage Server SDK with Spring Boot.
+ */
 package com.vonage.sample.serversdk.springboot;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.boot.web.server.ConfigurableWebServerFactory;
-import org.springframework.boot.web.server.WebServerFactoryCustomizer;
-import org.springframework.context.annotation.Bean;
-
-@SpringBootApplication
-@ConfigurationPropertiesScan
-public class Application {
-	public static void main(String[] args) {
-		var context = SpringApplication.run(Application.class, args);
-	}
-}
