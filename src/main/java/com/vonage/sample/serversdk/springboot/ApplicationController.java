8a1483ee2ef845388d1be99a662a938495aeba61
Sina Madani <sina.madani@vonage.com>
2024-11-07 10:05:26 +0000

    Update Maven debug parameters

--- src/main/java/com/vonage/sample/serversdk/springboot/ApplicationController.java
+++ src/main/java/com/vonage/sample/serversdk/springboot/ApplicationController.java
@@ -22,19 +22,8 @@
 package com.vonage.sample.serversdk.springboot;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
-import javax.tools.*;
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-import java.io.StringWriter;
-import java.net.URI;
-import java.net.URLClassLoader;
-import java.util.Arrays;
-import java.util.List;
 
 @Controller
 public class ApplicationController extends VonageController {
@@ -43,5 +32,12 @@ public class ApplicationController extends VonageController {
 	@GetMapping("/_/health")
 	public String health() {
 		return standardWebhookResponse();
+	}
+
+	@ResponseBody
+	@GetMapping("/_/metrics")
+	public String metrics() {
+		// TODO implement
+		return health();
 	}
 }
