c907640eebc7ed9dd6dd01eb33273f7bfeda896e
Sina Madani <sina.madani@vonage.com>
2024-11-07 11:10:36 +0000

    Disable Spring autoconfig logging

--- create_package.sh
+++ create_package.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
-zip -vr app.zip . -x "app.zip" -x "*.DS_Store" -x ".git/*" -x "*.git" -x ".env" -x ".github/*" -x ".idea/*" -x "mvnw.cmd" -x "gradlew.bat" -x "create_package.sh" -x ".gitignore"
+zip -r "app.zip" ./* \
+    -x "app.zip" \
+    -x "*.DS_Store" \
+    -x ".git/*" \
+    -x ".env" \
+    -x ".github/*" \
+    -x ".idea/*" \
+    -x "mvnw.cmd" \
+    -x "gradlew.bat" \
+    -x "create_package.sh" \
+    -x ".gitignore"
